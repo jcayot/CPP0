8fc0df8b6fbabd42962a7853d2c961caf8a65f64
jules <jules.cayot@ik.me>
2024-04-28 20:03:57 +0300

    Nothing

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Account.hpp"
+
+int Account::getNbAccounts(void) {
+	return 0;
+}
+
+int Account::getTotalAmount(void) {
+	return 0;
+}
+
+int Account::getNbDeposits(void) {
+	return 0;
+}
+
+int Account::getNbWithdrawals(void) {
+	return 0;
+}
+
+void Account::displayAccountsInfos(void) {
+
+}
+
+Account::Account(int initial_deposit) {
+
+}
+
+Account::~Account(void) {
+
+}
+
+void Account::makeDeposit(int deposit) {
+
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+	return false;
+}
+
+int Account::checkAmount(void) const {
+	return 0;
+}
+
+void Account::displayStatus(void) const {
+
+}
+
+void Account::_displayTimestamp(void) {
+
+}
+
+Account::Account(void) {
+
+}
